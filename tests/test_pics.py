2674f36c2687d7dbde93475a3ea9ea613e23e3e3
nnnLik <rslmakhmudov@gmail.com>
2023-09-25 11:01:33 +0300

    [ADD] test

--- tests/test_pics.py
+++ tests/test_pics.py
@@ -3,6 +3,8 @@ from typing import List, Dict
 import pytest
 import json
 
+import requests
+
 
 @pytest.fixture(scope="module")
 def image_directory_path():
@@ -42,6 +44,38 @@ def create_image_structure_dict(
     return image_structure
 
 
+def get_image_urls(english_words_data: Dict[str, Dict]) -> List[str]:
+    image_urls = []
+
+    for level, categories in english_words_data.items():
+        for category, words in categories.items():
+            for word_data in words:
+                image_url = word_data.get("image")
+                if image_url:
+                    image_urls.append((word_data["word"], image_url))
+
+    return image_urls
+
+
+@pytest.fixture(scope="module")
+def image_urls(english_words_data):
+    return get_image_urls(english_words_data)
+
+
+def test_image_urls_status(image_urls):
+    errors = []
+
+    for word, url in image_urls:
+        response = requests.get(url)
+        if response.status_code != 200:
+            errors.append(f"Error for word '{word}': Status {response.status_code}")
+
+    if errors:
+        for error in errors:
+            print(error)
+        pytest.fail("Some image URLs returned non-200 status codes")
+
+
 def test_image_filename_matches_word(
     image_directory_path: str, english_words_data: Dict[str, Dict]
 ):
@@ -62,7 +96,7 @@ def test_image_filename_matches_word(
     assert not missing_words, "\n".join(missing_words)
 
 
-def test_word_image_count_matches(
+def test_word_image_count_matches_1(
     image_directory_path: str, english_words_data: Dict[str, Dict]
 ):
     image_structure = create_image_structure_dict(image_directory_path)
@@ -82,3 +116,21 @@ def test_word_image_count_matches(
     assert not mismatched_counts, "Mismatched word and image counts:\n" + "\n".join(
         mismatched_counts
     )
+
+
+def test_word_image_count_matches(
+    image_directory_path: str, english_words_data: Dict[str, Dict]
+):
+    image_structure = create_image_structure_dict(image_directory_path)
+    total_words = 0
+    total_images = 0
+
+    for level, categories in english_words_data.items():
+        for category, words in categories.items():
+            total_words += len(words)
+            word_images = image_structure.get(level.upper(), {}).get(category, [])
+            total_images += len(word_images)
+
+    assert (
+        total_words == total_images
+    ), f"Total words: {total_words}, Total images: {total_images}"
